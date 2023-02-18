531bb25a4550528c28b9c8e02abc06d2bd94c21e
RashedBarat <rashedbarat@gmail.com>
2023-02-18 21:53:11 +0600

    first commit

---
+++ src/main/java/Core/FilePathHelper.java
@@ -0,0 +1,15 @@
+package Core;
+
+import java.io.File;
+
+public class FilePathHelper {
+
+    public static final String dir = System.getProperty("user.dir");
+    static File f = new File(dir);
+    static String filepath = f.getParent();
+    public static final String FileBathInCore = filepath+"/ApiTesting/src/main/java/repository/localRepo/";
+
+    public static final String postApiPath = FileBathInCore+"post.json";
+
+
+}
